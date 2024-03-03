6c91c7f5385588821e284c698cbdffe984afecf4
Ian Lawrie <iplawrie@mtu.edu>
2024-03-03 17:30:29 -0500

    added camera code

--- photobooth.py
+++ photobooth.py
@@ -1,5 +1,14 @@
 from time import sleep
+from picamera import PiCamera
 
 if __name__ == '__main__':
+    with PiCamera(resolution = (1920, 1080), framerate = 30) as camera:
+        camera.annotate_text_size = 30
+        camera.start_preview()
+        sleep(1)
+        for i in range(1, 6):
+            camera.annotate_text = i
+            sleep(1)
+        camera.stop_preview()
     print("exit")
     exit(0)
