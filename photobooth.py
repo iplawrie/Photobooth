73cc47930db98a1abce12bebbf862836ab7c1b68
Ian Lawrie <iplawrie@mtu.edu>
2024-03-03 19:19:58 -0500

    changed to picamera2

--- photobooth.py
+++ photobooth.py
@@ -1,14 +1,13 @@
 from time import sleep
-from picamera import PiCamera
+from picamera2 import Picamera2, Preview
 
 if __name__ == '__main__':
-    with PiCamera(resolution = (1920, 1080), framerate = 30) as camera:
-        camera.annotate_text_size = 30
+    with Picamera2() as camera:
+        camera_config = camera.create_preview_configuration()
+        camera.configure(camera_config)
         camera.start_preview()
-        sleep(1)
-        for i in range(1, 6):
-            camera.annotate_text = i
-            sleep(1)
-        camera.stop_preview()
+        camera.start()
+        sleep(5)
+        
     print("exit")
     exit(0)
