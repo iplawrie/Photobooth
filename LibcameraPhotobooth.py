85b223be7795b7445518bada7935af1fd1342126
Ian Lawrie <iplawrie@mtu.edu>
2024-03-10 18:53:43 -0400

    get it working

--- LibcameraPhotobooth.py
+++ LibcameraPhotobooth.py
@@ -1,13 +1,16 @@
-from time import sleep
+import sys
+from time import sleep, strftime
 from picamera2 import Picamera2, Preview
 
 if __name__ == '__main__':
     with Picamera2() as camera:
         camera_config = camera.create_preview_configuration()
         camera.configure(camera_config)
-        camera.start_preview()
+        camera.start_preview(Preview.QTGL)
         camera.start()
         sleep(5)
-        
+        metadata = camera.capture_file("/home/ian/Desktop/Photos/image%s.png" % strftime("%Y%m%d-%H%M%S"))
+        print(metadata)
+        camera.close()
     print("exit")
     exit(0)
