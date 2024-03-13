6821196b96256feaec47a95bd2e6fad451bb0303
Ian Lawrie <iplawrie@mtu.edu>
2024-03-12 20:28:19 -0400

    create test for pi

--- PicameraPhotobooth.py
+++ PicameraPhotobooth.py
@@ -1,13 +1,20 @@
-from gpiozero import Button
-from PIL import Image
+from PhotoBooth import PhotoBooth
+#from gpiozero import Button
+from time import sleep
 
 button = Button(17)
-def main(camera):
-    while True:
-        input("Button Press")
-        # button.wait_for_press()
-
+def main():
+    location = "/home/ian/Desktop/Photos"
+    pb = PhotoBooth(location)
+    pb.camerapreview()
+    input("Button Press")
+    img = pb.take_pic()
+    pb.imagepreview(img)
+    sleep(3)
+    pb.imageremove()
+    sleep(5)
+    pb.close()
 
 if __name__ == '__main__':
-    main(camera)
+    main()
 
