8cffc3545b1e381b51b3ff179729db4191f36a7f
Ian Lawrie <iplawrie@mtu.edu>
2024-03-14 15:01:50 -0400

    seperated text countdown to seperate function. added preview text functions

--- PhotoBooth.py
+++ PhotoBooth.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 from time import sleep
+from PIL import Image
 class PhotoBooth:
     camera = PiCamera()
     shotnumber = 0
@@ -8,31 +9,38 @@ class PhotoBooth:
 
     def __init__(self, location):
         self.location = location
+        self.camera.annotate_text_size = 160
         #self.camera.resolution = (1280, 720)
 
     def close(self):
         self.camera.stop_preview()
-        sleep(.5)
         self.camera.close()
+
+    def text(self, text):
+        self.camera.annotate_text = text
+
+    def clear_text(self):
+        self.camera.annotate_text = ''
+
+    def text_countdown(self, count):
+        for i in reversed(range(count)):
+            self.text(count + 1)
+            sleep(1)
+        self.text("Cheese")
+        self.clear_text()
 
     def take_pic(self):
         imagelocation = self.location + "/image%s.png" % self.shotnumber
-        for j in reversed(range(5)):
-            self.camera.annotate_text = str(j + 1)
-            self.camera.annotate_text_size = 160
-            sleep(1)
-        self.camera.annotate_text = ''
-
         #location = "/home/ian/Desktop/Photos/image%s.jpg" % self.shotnumber
         self.camera.capture(imagelocation)
         image = Image.open(imagelocation)
         self.shotnumber += 1
         return image
 
-    def camerapreview(self):
+    def camera_preview(self):
         self.camera.start_preview()
 
-    def imagepreview(self, img):
+    def image_preview(self, img):
         pad = Image.new('RGB', (
             ((img.size[0] + 31) // 32) * 32,
             ((img.size[1] + 15) // 16) * 16,
@@ -42,5 +50,5 @@ class PhotoBooth:
         self.imageoverlay.alpha = 255
         self.imageoverlay.layer = 3
 
-    def imageremove(self):
+    def image_remove(self):
         self.camera.remove_overlay(self.imageoverlay)
