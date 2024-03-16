32abb6c6624b85cf5c5fb808edb35bcd4cd8b213
Ian Lawrie <iplawrie@mtu.edu>
2024-03-16 19:28:38 -0400

    testing

--- Picamera/PicameraPhotobooth.py
+++ Picamera/PicameraPhotobooth.py
@@ -1,22 +1,29 @@
 from PhotoBooth import PhotoBooth
-#from gpiozero import Button, LED
+from gpiozero import Button, LED
 from time import sleep
+from signal import pause
 
-#button = Button(17)
+button = Button(26)
 #exitbutton = Button()
 #buttonlight = LED()
 
 def main():
     pb = PhotoBooth()
-    pb.camera_preview()
-    input("Button Press")
-    pb.text_countdown(5)
-    img = pb.take_pic()
-    pb.image_preview(img)
-    sleep(3)
-    pb.image_remove()
-    sleep(5)
-    pb.close()
+    button.when_pressed = pb.image_effects
+    try:
+        pb.camera_preview()
+    #input("Button Press")
+        #button.wait_for_press()
+        #pb.text_countdown(5)
+        #img = pb.take_pic()
+    #pb.image_preview(img)
+    #sleep(3)
+    #pb.image_remove()
+        #pb.text_countdown(5)
+        #sleep(5)
+        pause()
+    finally:
+        pb.close()
 
 if __name__ == '__main__':
     main()
