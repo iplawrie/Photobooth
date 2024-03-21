22167effb46d203c7800e2022f9e6e4bb142f544
Ian Lawrie <iplawrie@mtu.edu>
2024-03-21 11:07:25 -0400

    something was changed

--- Picamera/PicameraPhotobooth.py
+++ Picamera/PicameraPhotobooth.py
@@ -3,13 +3,15 @@ from gpiozero import Button, LED
 from time import sleep
 from signal import pause
 
+Button.was_held = False
 button = Button(26)
 #exitbutton = Button()
 #buttonlight = LED()
 
 def main():
     pb = PhotoBooth()
-    button.when_pressed = pb.image_effects
+    button.when_pressed = pb.image_effects()
+
     try:
         pb.camera_preview()
     #input("Button Press")
